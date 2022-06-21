02bf738ab869eb395e4762d8643c09dc4a2d37c8
facundo <facuvega1989@gmail.com>
2022-06-21 18:20:55 -0300

    added cypress.env.json.example , added README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+INSTRUCTIONS:
+1) install node.js
+2) unzip repository
+3) command npm install
+4) create a cpress.env.json based on cypress.env.json.example and complete with your sketch prod credentials
+
+
+RUNS: on project root.
+1) cy:run to run E2E headless
+2) cy:open to open Cypress interactive console
+
+
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,40 +5,34 @@ import * as Data from '../fixtures/data.json'
 import * as Assert from '../support/helper/assert'
 
 
-describe('Sketch UI Login Tests', () => {  
-
-  it('Log in - Email format validation ', () => {
-    cy.visit('/') 
+describe('Sketch UI Login Tests', () => {
+  beforeEach(() => {
+    cy.visit('/')
     SignInActions.goToSignInPage();
-    LoginActions.loginWithoutSSO("thisIsNotAFormatEmailInput.com",Data.userPassword)
+  })
+
+  it('Log in - Email format validation ', () => {    
+    LoginActions.loginWithoutSSO("thisIsNotAFormatEmailInput.com",Cypress.env("userPassword"))
     Assert.contains(LoginActions.getValidationMessage(), Data.validationMessages.noEmailFormatAlert)
   })
 
   it('Log in - Blank email Validation', () => {
-    cy.visit('/')
-    SignInActions.goToSignInPage();
-    LoginActions.fillPasswordField(Data.userPassword);
+    LoginActions.fillPasswordField(Cypress.env("userPassword"));
     Assert.contains(LoginActions.getValidationMessage(Data.validationMessages.blankEmailAlert), Data.validationMessages.blankEmailAlert)
   })
 
   it('Log in - Blank password Validation', () => {
-    cy.visit('/')
-    SignInActions.goToSignInPage();
-    LoginActions.fillEmailField(Data.userEmail);
+    LoginActions.fillEmailField(Cypress.env("userEmail"));
     Assert.contains(LoginActions.getValidationMessage(Data.validationMessages.BlankPasswordAlert), Data.validationMessages.BlankPasswordAlert)
   })
 
   it('Log in - invalid credentials', () => {
-    cy.visit('/')
-    SignInActions.goToSignInPage();
     LoginActions.loginWithoutSSO(Data.invalidEmail,Data.invalidPassword)
     Assert.contains(LoginActions.getValidationMessage(),Data.validationMessages.invalidCredentials)
   })
 
-  it('Log in - Successful case ', () => {
-    cy.visit('/')
-    SignInActions.goToSignInPage();
-    LoginActions.loginWithoutSSO(Data.userEmail,Data.userPassword)
+  it('Log in - Successful case', () => {
+    LoginActions.loginWithoutSSO(Cypress.env("userEmail"),Cypress.env("userPassword"))
     Assert.contains(SidebarActions.getWorkspaceName(), Data.defaultWorkspace)
     Assert.eq(cy.title(),'Sketch - Documents')
   })
